37de8f4536bdb07d7cf440d47a6bf96c0b640908
Varga Antal <varianti.antika@gmail.com>
2021-10-19 12:19:14 +0200

    style

--- repter.php
+++ repter.php
@@ -6,10 +6,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <title>Gyuri</title>
+    <link rel="icon" href="img/airport.png" type="image/icon type">
 </head>
 <body>
     <header>
         <a href="https://www.youtube.com/watch?v=wDQ6pU5re-0"><h1>RepTér</h1></a>
+     <nav>
+         <ul>
+             <li><p>érkező</p></li>
+             <li><p>távozó</p></li>
+             <li><p>valami</p></li>
+         </ul>
+     </nav>
     </header>
     <div class="container">
         <div class="row">
