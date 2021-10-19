f3007a02317a865050165d7cb25e508552ffa304
Varga Antal <varianti.antika@gmail.com>
2021-10-19 10:54:26 +0200

    kezdetek

---
+++ repter.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Gyuri</title>
+</head>
+<body>
+    <header>
+        <a href="https://www.youtube.com/watch?v=wDQ6pU5re-0"><h1>RepTér</h1></a>
+    </header>
+    <div class="container">
+        <div class="row">
+            
+            <div class="col-md-4">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla architecto illo harum distinctio dicta, non veniam, deserunt cupiditate culpa reiciendis at libero iusto et nihil atque quibusdam ut ipsa error.</p>
+            </div>
+            <div class="col-md-4">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla architecto illo harum distinctio dicta, non veniam, deserunt cupiditate culpa reiciendis at libero iusto et nihil atque quibusdam ut ipsa error.</p>
+            </div>
+            <div class="col-md-4">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla architecto illo harum distinctio dicta, non veniam, deserunt cupiditate culpa reiciendis at libero iusto et nihil atque quibusdam ut ipsa error.</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
